9718931ba298dc9124a103fd8567ffa62fb3538f
Manu K <manuknarayanan@gmail.com>
2024-02-06 23:18:42 +0530

    feat(types): add feed types

---
+++ src/feat/feed/types/feedTypes.ts
@@ -0,0 +1,13 @@
+export interface Feed {
+  title?: string
+  nid_dont_use?: string
+  field_photo_image_section?: string
+  path?: string
+  nid?: string
+  photo_image_nids?: string
+  ImageStyle_thumbnail?: string
+  last_update?: number
+  views_count?: number
+  author_uid?: number
+  author_name?: string
+}
