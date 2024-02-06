8de15b81073e7bbe908ab21a648ab1400142448f
Manu K <manuknarayanan@gmail.com>
2024-02-06 13:24:40 +0530

    feat: Remove unused state and refactor QueryClient instantiation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { useState } from 'react'
 import './App.scss'
 
+const queryClient = new QueryClient()
 function App() {
-  const [count, setCount] = useState(0)
-  const queryClient = new QueryClient()
-
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
