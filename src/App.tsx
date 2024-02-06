30ca352fdb1f19d97cdbed81219dd666a93dd814
Manu K <manuknarayanan@gmail.com>
2024-02-06 23:12:03 +0530

    feat: Add and config NextUIProvider and Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { NextUIProvider } from '@nextui-org/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './App.scss'
+import { FeedList } from './feat/feed/components/FeedList'
 
 const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <></>
+      <NextUIProvider>
+        <FeedList />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </NextUIProvider>
     </QueryClientProvider>
   )
 }
